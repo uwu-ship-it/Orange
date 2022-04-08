7dc6be09a969f36d98bfcabf81d8c1948ab87d1c
Nixeff <theodore.folkesson@gmail.com>
2022-04-08 12:01:40 +0200

    Lekt lite och fått hemta data funka hahah

---
+++ resultat.php
@@ -0,0 +1,4 @@
+<?php
+    $res = $_GET["rate"];
+    echo "$res";
+?>
---
+++ test.php
@@ -0,0 +1,39 @@
+<?php
+    $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "gameon";
+
+        // Create Connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        $anv = $_GET['Namn'];
+        $pass = $_GET['Pass'];
+
+        $sql = "SELECT * FROM person WHERE Namn='$anv' and Password='$pass'";
+        $result = $conn->query($sql);
+    
+        if ($result->num_rows > 0) {
+
+            //Output rows as long as they exists
+            while ($row = $result->fetch_assoc()) {
+                $id = $row['ID'];
+            }
+        }
+        else
+        echo "Det finns ingen data";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=§, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="resultat.php" method="GET">
+        <input type="range" name="rate" min="1" max="10">
+        <input type="submit"> 
+    </form>
+</body>
+</html>
