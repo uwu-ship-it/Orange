4d13012997c4be0c0110af497c24f07c00ff5e47
AsterN037 <klara.sunnergren@elev.ga.ntig.se>
2022-05-06 11:14:27 +0200

    Css

---
+++ anvandarsidan.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>användarsidan</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<header>
+  <img id = "logga" src='grafik/ntil.jpg'>
+</header>
+<div class="sidenav">
+  <a href="spelval.php">Spel</a>
+  <a href="#">Grupp1</a>
+  <a href="#">Grupp2</a>
+  <a href="#">Grupp3</a>
+  <a href="#">Grupp4</a>
+  <a href="#">Grupp5</a>
+  <a href="#">Logga ut</a>
+</div>
+
+<?php
+
+?>
+</body>
+</html>
